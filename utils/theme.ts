d79d142de55d359e10e10e10e0d839d9217f7820
VarunLanjhara <lanjharav@gmail.com>
2022-06-26 18:36:45 +0530

    login page ui stuff

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,9 +1,16 @@
 import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import "@fontsource/open-sans/500.css";
 
 const config: ThemeConfig = {
   initialColorMode: "light",
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  ...config,
+  fonts: {
+    body: `'Open-Sans', sans-serif`,
+    heading: `'Open-Sans', sans-serif`,
+  },
+});
 export default theme;
